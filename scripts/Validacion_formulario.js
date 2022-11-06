9281a2388433d116848e5276f2a05efdf0f2a99b
abrilazc <jsazcarate@fi.uba.ar>
2022-11-06 17:51:08 -0300

    script validation fix wip

--- scripts/Validacion_formulario.js
+++ scripts/Validacion_formulario.js
@@ -1,17 +1,23 @@
 function validarFormulario(){
     const motivo=document.getElementById("motivo");
-    const correo=document.getElementById("correo");
+    const correo=document.getElementById("email");
+    let error=false;
     if (!correo.checkValidity()){
         document.getElementById("correoError").innerHTML="Inserte una direccion de correo valida";
+        error=true;
     }
     else{
         document.getElementById("correoError").innerHTML="";
     }
     if(motivo=="sel_motivo"){
         document.getElementById("motivoError").innerHTML="Inserte un motivo";
+        error=true;
     }
     else{
         document.getElementById("motivoError").innerHTML="";
+    }
+    if(!error) {
+        document.getElementById("envioCorrecto").innerHTML="Su mensaje se ha enviado correctamente";
     }
     return;
 }
